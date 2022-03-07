3f1e69d98fe2d993cd5833e7b6c1bba667e94b83
leejy2296 <leejy12341266@gmail.com>
2022-03-07 11:00:53 +0900

    와이파이연결+blink 아두이노

--- wifi/wifi.ino
+++ wifi/wifi.ino
@@ -21,7 +21,14 @@ void setup()
   /* connected ip address : 아이피 주소 출력 */
   Serial.print("Connected, IP address: ");
   Serial.println(WiFi.localIP());
-  
+
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
-void loop() {}
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                       // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000); 
+  }
